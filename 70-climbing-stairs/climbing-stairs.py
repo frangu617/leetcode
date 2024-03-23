d69d43c4a0139a1250af3fd7d9928a1bb440fe6c
Francisco E Gutierrez <142462584+frangu617@users.noreply.github.com>
2024-03-23 13:23:04 -0700

    Time: 40 ms (18.99%) | Memory: 16.6 MB (5.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,18 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Base cases
         if n == 1:
             return 1
         if n == 2:
             return 2
         
-        # Initialize an array to store the number of distinct ways to reach each step
-        dp = [0] * (n + 1)
-        dp[1] = 1  # There's 1 way to climb to step 1
-        dp[2] = 2  # There are 2 ways to climb to step 2 (1 step + 1 step or 2 steps)
+        prev1 = 1  # Number of ways to reach step 1
+        prev2 = 2  # Number of ways to reach step 2
         
-        # Calculate the number of distinct ways to climb to each step
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]  # Number of ways to reach step i is sum of ways to reach step (i-1) and (i-2)
+        for _ in range(3, n + 1):
+            current = prev1 + prev2
+            prev1, prev2 = prev2, current
         
-        return dp[n]  # Return the number of distinct ways to climb to the top
+        return prev2
